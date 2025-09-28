cbef2367b4209920a11f807d013adeba0b7dce46
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-09-28 13:15:29 +0900

    feat

--- modules/service.tf
+++ modules/service.tf
@@ -1,7 +1,19 @@
 resource "google_project_service" "default" {
+  for_each = toset([
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "container.googleapis.com",
+    "secretmanager.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "clouddeploy.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "sqladmin.googleapis.com"
+  ])
+
   disable_dependent_services = null
   disable_on_destroy         = null
   project                    = "cloudrun-hands-on-473403"
-  service                    = "artifactregistry.googleapis.com"
+  service                    = each.value
 }
 
