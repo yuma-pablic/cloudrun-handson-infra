0cc4568da930e0430372631872f765b9ef4e5d6d
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-09-30 00:17:30 +0900

    fix

--- modules/service_account.tf
+++ modules/service_account.tf
@@ -37,3 +37,45 @@ resource "google_project_iam_member" "service_github" {
   project = "cloudrun-hands-on-473403"
   role    = "roles/secretmanager.secretAccessor"
 }
+
+resource "google_service_account" "cnsrun-clouddeploy" {
+  account_id   = "cnsrun-clouddeploy"
+  display_name = "Service Account for Cloud Deploy in cnsrun"
+}
+
+resource "google_project_iam_member" "log_writer" {
+  member  = google_service_account.cnsrun-clouddeploy.member
+  project = "cloudrun-hands-on-473403"
+  role    = "roles/logging.logWriter"
+}
+
+resource "google_project_iam_member" "cloud_deploy_job_runner" {
+  member  = google_service_account.cnsrun-clouddeploy.member
+  project = "cloudrun-hands-on-473403"
+  role    = "roles/clouddeploy.jobRunner"
+}
+
+
+resource "google_project_iam_member" "cloud_deploy_service_account" {
+  member  = google_service_account.cnsrun-clouddeploy.member
+  project = "cloudrun-hands-on-473403"
+  role    = "roles/iam.serviceAccountUser"
+}
+
+resource "google_project_iam_member" "run_developer" {
+  member  = google_service_account.cnsrun-clouddeploy.member
+  project = "cloudrun-hands-on-473403"
+  role    = "roles/run.developer"
+}
+
+resource "google_project_iam_member" "object_user" {
+  member  = google_service_account.cnsrun-clouddeploy.member
+  project = "cloudrun-hands-on-473403"
+  role    = "roles/storage.objectUser"
+}
+
+
+
+
+
+
