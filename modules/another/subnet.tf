c830c61accc0e3dd24bcf959b33d9b29abf3df86
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-10-05 13:55:03 +0900

    fix

---
+++ modules/another/subnet.tf
@@ -0,0 +1,7 @@
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name                     = "cnsrun-cloudrun-hands-on-473403"
+  ip_cidr_range            = "10.0.0.0/24"
+  region                   = "asia-northeast1"
+  network                  = google_compute_network.vpc.id
+  private_ip_google_access = true
+}
