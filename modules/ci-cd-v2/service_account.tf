47e185840824d372b0cdcdcef68c148f2ada96b6
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-10-18 16:08:23 +0900

    feat

--- modules/ci-cd-v2/service_account.tf
+++
@@ -1,4 +0,0 @@
-resource "google_service_account" "app" {
-  account_id   = "cnsrun-app-${var.app}"
-  display_name = "Service Account for cnsrun-${var.app}"
-}
--- modules/cloud-run/iam_member.tf
+++ modules/cloud-run/iam_member.tf
@@ -1,8 +1,5 @@
-# resource "google_cloud_run_service_iam_member" "editor" {
-#   location = "asia-northeast1"
-#   member   = "allUsers"
-#   project  = "cloudrun-hands-on-473403"
-#   role     = "roles/run.invoker"
-#   service  = "v1/projects/cloudrun-hands-on-473403/locations/asia-northeast1/services/cnsrun-frontend"
-# }
-
+resource "google_project_iam_member" "app" {
+  member  = google_service_account.app.member
+  project = var.project_id
+  role    = "roles/run.invoker"
+}
