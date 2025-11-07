3cb60b5aa85facf8f97c9c3b455c3f894926d1ce
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-11-08 01:04:21 +0900

    feat

--- modules/load_balancer/ssl_certificate.tf
+++ modules/load_balancer/ssl_certificate.tf
@@ -1,9 +1,32 @@
 resource "google_compute_ssl_certificate" "default" {
   name_prefix = "cnsrun-certificate"
-  private_key = file("${path.root}/private.key")
-  certificate = file("${path.root}/cnsrun.crt")
+  private_key = tls_private_key.alb.private_key_pem
+  certificate = tls_self_signed_cert.alb.cert_pem
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "tls_private_key" "alb" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "tls_self_signed_cert" "alb" {
+  private_key_pem = tls_private_key.alb.private_key_pem
+  subject {
+   country             = "JP"
+    province            = "Kanagawa"
+    locality            = "Yokohama"
+    organization        = "uma-arai"
+    organizational_unit = "Container"
+    common_name         = "team.bit.uma.arai@gmail.com"
+  }
+  validity_period_hours = 87600
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth"
+  ]
 }
